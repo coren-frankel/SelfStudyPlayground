bffbac0957e887b7a7b4429ab3a6740d95eba50f
unclebabykern <coren.frankel@gmail.com>
2022-10-31 15:14:06 -0600

    Redux practice

--- React+Redux/counter/src/index.js
+++ React+Redux/counter/src/index.js
@@ -1,17 +1,29 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+//DEPRACATED THIS ISN'T WORKING
+import { createStore } from 'redux';
+import Counter from './components/Counter';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const initialState = {
+    count: 0
+};
+// Reducer function
+function reducer(state = initialState, action) {
+    switch (action.type) {
+        case 'INCREMENT':
+            return { count: state.count + action.num };
+        default:
+            return state;
+    }
+}
+
+const store = createStore(reducer);
+const el = <Provider store={store}>
+    <Counter />
+</Provider>;
+
+ReactDOM.render(
+    el,
+    document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
