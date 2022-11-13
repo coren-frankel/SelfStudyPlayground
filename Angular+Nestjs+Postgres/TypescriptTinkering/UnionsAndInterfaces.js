b65eba39dc1f59fbc59a9f94a1b89b437ef89c30
unclebabykern <coren.frankel@gmail.com>
2022-11-13 07:32:54 -0700

    NestJS Cars API

---
+++ Angular+Nestjs+Postgres/TypescriptTinkering/UnionsAndInterfaces.js
@@ -0,0 +1,4 @@
+var me = { name: 'coren', age: 31 };
+var you = { name: 'leap day william', age: 'seven' };
+console.log("Hi my name is ".concat(me.name, " and I am ").concat(me.age, " years old."));
+console.log("Hi my name is ".concat(you.name, " and I am ").concat(you.age, " years old."));
--- Angular+Nestjs+Postgres/TypescriptTinkering/UnionsAndInterfaces.ts
+++ Angular+Nestjs+Postgres/TypescriptTinkering/UnionsAndInterfaces.ts
@@ -2,11 +2,13 @@
 
 
 
-interface int {
-    name : string
-    age : number | string
+interface IUser {
+    name : string;
+    age : number | string;
 }
 
-let coren : int = {name : 'coren', age : 31}
+let me : IUser = {name : 'coren', age : 31}
+let you : IUser = {name : 'leap day william', age: 'seven'}
 
-console.log(`Hi my name is ${coren.name} and I am ${coren.age} years old.`)
+console.log(`Hi my name is ${me.name} and I am ${me.age} years old.`)
+console.log(`Hi my name is ${you.name} and I am ${you.age} years old.`)
